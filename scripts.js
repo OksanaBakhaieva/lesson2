24401713f9333a5fbef8bc10f6f4a2b748c52996
Oksana Bakhaieva <obahaeva@gmail.com>
2024-10-08 16:20:38 +0100

    Added js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+
+document.querySelector(".title-link").addEventListener('click', hideImageFunction)
+
+function hideImageFunction(e) {
+    // e.preventDefault();
+    document.querySelector('.food-img').classList.add('hidden')
+}
+
