290ba56c5ee7a57018d9c4ba7514bbd4be7aaec7
Oksana Bakhaieva <obahaeva@gmail.com>
2024-10-08 16:35:41 +0100

    Updated js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
 console.log("Hello world");
 
-document.querySelector(".title-link").addEventListener('click', hideImageFunction)
+// document.querySelector('h1').addEventListener('click', hideImageFunction)
 
-function hideImageFunction(e) {
-    // e.preventDefault();
-    document.querySelector('.food-img').classList.add('hidden')
-}
+// function hideImageFunction(e) {
+//     // e.preventDefault();
+//     document.querySelector('.food-img').classList.add('hidden')
+// }
 
+// ==================
+const title = document.querySelector('h1')
+const image = document.querySelector('img')
+
+title.addEventListener('click', function() {
+    if (image.style.display == 'none') {
+        image.style.display = 'block'
+    } else {
+        image.style.display = 'none'
+    }
+})
